fec9e7664ac55cae547e6b9138e0e722ff2d73ee
Brendon Machado <benmacario.s@gmail.com>
2020-07-01 21:56:29 -0300

    Adicionando e removendo itens

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -14,15 +14,38 @@ class TechList extends Component {
     this.setState({ newTech: e.target.value })
   }
 
+  handleSubmit = e => {
+    e.preventDefault()
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    })
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
     return (
-      <>
-      <h1>{this.state.newTech}</h1>
-      <ul>
-        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
-      </ul>
-      <input type="text" onChange={this.handleInputChange}/>
-      </>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech =>
+            <li key={tech}>
+              {tech + ' '}
+              <button onClick={() => this.handleDelete(tech)} type="button">
+                Remover
+              </button>
+            </li>)
+          }
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     )
   }
 }
